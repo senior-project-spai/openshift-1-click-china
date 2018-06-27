11834e6b59f507b6c751fb092be1783eb04e0704
xiaohua.pan <pxhua@yahoo.cn>
2018-06-27 22:31:53 +0800

    create allinone install repo

---
+++ README.md
@@ -0,0 +1,15 @@
+一键部署Openshift 3.9
+操作步骤如下：
+#### 1. 准备一台主机(CentOS 7.4以上, CPU > 2core, Memory > 4G)
+
+#### 2. 将一键部署脚本拷贝到主机上
+
+#### 3. cd到openshift-oneclick-allinone目录
+    
+    cd openshift-oneclick-allinone
+    
+#### 4. 运行部署
+    
+    /bin/bash deploy_openshift.sh
+    
+#### 5. 浏览器访问
---
+++ deploy_openshift.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export CHANGEREPO=true
+if [ $CHANGEREPO == true -a ! -d /etc/yum.repos.d/back ]
+then
+    cd /etc/yum.repos.d/; mkdir -p back; mv -f *.repo back/; cd -
+    cp files/all.repo /etc/yum.repos.d/
+    yum clean all
+fi
+current_path=`pwd`
+yum install ansible -y
+ansible-playbook playbook.yml
+cd $current_path/openshift-ansible-playbook
+ansible-playbook playbooks/prerequisites.yml
+ansible-playbook playbooks/deploy_cluster.yml
+htpasswd -b /etc/origin/master/htpasswd admin admin
+oc adm policy add-cluster-role-to-user cluster-admin admin
