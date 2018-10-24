4c1cd2db3b814cf906f6608f75390bb35692303d
xhua <pxhua@aliyun.com>
2018-10-24 20:13:04 +0800

    更新 deploy_openshift.sh

--- deploy_openshift.sh
+++ deploy_openshift.sh
@@ -7,7 +7,7 @@ then
     yum clean all
 fi
 current_path=`pwd`
-yum localinstall tools/ansible-2.4.6.0-1.el7.ans.noarch.rpm
+yum localinstall tools/ansible-2.4.6.0-1.el7.ans.noarch.rpm -y
 ansible-playbook playbook.yml
 cd $current_path/openshift-ansible-playbook
 ansible-playbook playbooks/prerequisites.yml
