d9082a6eb7863cbf44952eeaec5ec7a66086ba3b
xiaohua.pan <pxhua@yahoo.cn>
2018-11-13 13:44:59 +0800

    up for template

--- files/init_openshift.sh
+++ files/init_openshift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OPENSHIFT_TEMPLATE_DIR='./openshift-templates'
-CICD_TEMPLATES = ( gogs jenkins )
+CICD_TEMPLATES=(gogs jenkins gitlab nexus3 sonarqube)
 
 # create project CI
 oc get project cicd; if [ $? -gt 0 ]; then oc new-project cicd --display-name="CICD自动化"; fi
@@ -14,7 +14,7 @@ do
 	oc get template $temp_name -n cicd
 	if [ $? -gt 0 ]
 	then
-		oc create -f ${OPENSHIFT_TEMPLATE_DIR}/${temp_name}-template.yml -n cicd
+		oc create -f ${OPENSHIFT_TEMPLATE_DIR}/${temp_name}-template.yaml -n cicd
 	fi
 done
 
