8ebfa4f19add8521c621c58544e2f41587c590f8
xiaohua.pan <pxhua@yahoo.cn>
2018-11-17 14:33:27 +0800

    add pull image mysql57

--- files/pull_cicd_images.sh
+++ files/pull_cicd_images.sh
@@ -5,7 +5,8 @@ CICD_IMAGES=(gitlab/gitlab-ce:11.4.0-ce.0 \
 	openshiftdemos/gogs:0.11.34 \
 	openshift/jenkins-2-centos7:latest \
 	sonatype/nexus3:3.14.0 \
-	openshiftdemos/sonarqube:6.7)
+	openshiftdemos/sonarqube:6.7 \
+	centos/mysql-57-centos7:latest )
 
 for image in "${CICD_IMAGES[@]}"
 do
