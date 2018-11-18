77aafe4562f3be570393fdccf30ceef791f191ab
xiaohua.pan <pxhua@yahoo.cn>
2018-11-18 22:05:26 +0800

    mv jenkins to openshift

--- files/pull_cicd_images.sh
+++ files/pull_cicd_images.sh
@@ -5,6 +5,7 @@ CICD_IMAGES=(gitlab/gitlab-ce:11.4.0-ce.0 \
 	openshiftdemos/gogs:0.11.34 \
 	openshift/jenkins-2-centos7:latest \
 	sonatype/nexus3:3.14.0 \
+	openshift/jenkins-agent-maven-35-centos7:v4.0 \
 	openshiftdemos/sonarqube:6.7 \
 	centos/mysql-57-centos7:latest )
 
