755697f62b433ce02358f3b59fc0172893c7d6dc
xiaohua.pan <pxhua@yahoo.cn>
2018-11-21 14:07:42 +0800

    up for allinone webconsole.js

--- openshift-templates/allinone-webconsole.js
+++ openshift-templates/allinone-webconsole.js
@@ -6,4 +6,11 @@
 	window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[4].label="存储"
 	window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[5].label="监控"
 	window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[6].label="商店"
+	window.OPENSHIFT_CONSTANTS.APP_LAUNCHER_NAVIGATION = [
+	{
+	  title: "Sharing Videos",
+	  iconClass: "fa fa-video-camera",
+	  href: "https://yun.baidu.com/s/1xIwYILHQebEHZOcW4yvsAw",
+	  tooltip: "一键部署Openshift相关视频"
+	}];
 }());
