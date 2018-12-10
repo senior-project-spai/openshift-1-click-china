6b419e91538dfab0b7b449774249dbc8376e39d9
xiaohua.pan <pxhua@yahoo.cn>
2018-12-10 17:48:27 +0800

    up for pipeline

--- files/init_cicd.sh
+++ files/init_cicd.sh
@@ -20,9 +20,10 @@ else
 	git clone https://gitee.com/openshiftx/jeesite.git
 	cd jeesite
 fi
-oc get bc jeesite > /dev/null 2>&1 || cat Dockerfile | oc new-build -D - --name jeesite -n jeesite
-oc get bc jeesite-pipeline > /dev/null 2>&1 || oc create -f openshift-pipeline.yml
+oc get bc jeesite  -n jeesite > /dev/null 2>&1 || cat Dockerfile | oc new-build -D - --name jeesite -n jeesite
+oc get bc jeesite-pipeline  -n jeesite > /dev/null 2>&1 || oc create -f openshift-pipeline.yml -n jeesite
 
-oc get dc jeesite > /dev/null 2>&1 || oc new-app jeesite/jeesite --name=jeesite -n jeesite --allow-missing-imagestream-tags
+oc get serviceaccount jeesite -n jeesite 2>&1 || oc create serviceaccount jeesite -n jeesite
+oc get dc jeesite > /dev/null 2>&1 || oc create -f ../openshift-templates/jeesite-deployment.yaml -n jeesite
 oc get svc jeesite > /dev/null 2>&1 || oc expose dc jeesite --port=8080
 oc get route jeesite > /dev/null 2>&1 || oc expose svc jeesite
