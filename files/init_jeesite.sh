e8db3d1a521e7a14be3877f55b87b5ecb387c036
xiaohua.pan <pxhua@yahoo.cn>
2018-12-28 14:20:52 +0800

    fix token

--- files/init_jeesite.sh
+++ files/init_jeesite.sh
@@ -23,12 +23,18 @@ then
 		cd jeesite
 	fi
 else
-	curl -s -H "Authorization: token 6200198c6fcb7dc28e5330be0ca5fdc31c0b2a2e"  -X POST http://gogs.cicd.svc:3000/api/v1/user/repos -d 'name=jeesite&private=false'
+	curl -s -H "Authorization: token $token"  -X POST http://gogs.cicd.svc:3000/api/v1/user/repos -d 'name=jeesite&private=false'
 	if [ ! -d jeesite ]
 	then
-		git clone https://gitee.com/openshiftx/jeesite.git
+		git clone https://gitee.com/openshiftx/jeesite.git jeesite_src
+		cd jeesite_src
+		git archive --format=zip master > master.zip
+		cd ..
+		mkdir jeesite
+		cd jeesite
+		unzip ../jeesite_src/master.zip 
+		rm -rf ../jeesite_src
 	fi
-	cd jeesite
 	git init
 	git add .
 	git commit -m "first commit"
@@ -39,16 +45,6 @@ fi
 
 oc get project jeesite  > /dev/null 2>&1 ||  oc new-project jeesite --display-name=JeeSite
 oc get dc mysql -n jeesite > /dev/null 2>&1 || oc new-app --template=openshift/mysql-persistent --name=mysql --param=MYSQL_USER=jeesite --param=MYSQL_PASSWORD=jeesite --param=MYSQL_ROOT_PASSWORD=jeesite --param=MYSQL_DATABASE=jeesite -n jeesite
-
-if [ -d jeesite ]
-then
-	cd jeesite
-	git pull
-else
-	git clone https://gitee.com/openshiftx/jeesite.git
-	cd jeesite
-fi
-
 
 oc get bc jeesite  -n jeesite > /dev/null 2>&1 || cat Dockerfile | oc new-build -D - --name jeesite -n jeesite && oc adm policy add-scc-to-user anyuid -z jeesite -n jeesite
 oc get bc jeesite-pipeline  -n jeesite > /dev/null 2>&1 || oc create -f openshift-pipeline.yml -n jeesite
