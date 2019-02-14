b7e0d64363ae3ca298ae16ba18252494ef4fdacc
xiaohua.pan <pxhua@yahoo.cn>
2019-02-14 17:27:39 +0800

    add istio operator install

---
+++ istio/docker-pull-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/istio-operator:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/openshift-ansible:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/citadel:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/proxyv2:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/pilot:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/mixer:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/galley:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/sidecar-injector:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/proxy-init:0.7.0
+docker pull registry.access.redhat.com/openshift-istio-tech-preview/kiali:0.13.0
+docker pull registry.access.redhat.com/distributed-tracing-tech-preview/jaeger-elasticsearch:5.6.10
+docker pull registry.access.redhat.com/distributed-tracing-tech-preview/jaeger-agent:1.9.0
+docker pull registry.access.redhat.com/distributed-tracing-tech-preview/jaeger-collector:1.9.0
+docker pull registry.access.redhat.com/distributed-tracing-tech-preview/jaeger-query:1.9.0
+docker pull docker.io/grafana/grafana:5.4.0
+docker pull docker.io/prom/prometheus:v2.3.1
