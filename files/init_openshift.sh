19cfb78c5a883c4e9800c01ba32ae2d62b1aa338
xiaohua.pan <pxhua@yahoo.cn>
2019-03-06 17:42:36 +0800

    add k-elk

--- files/init_openshift.sh
+++ files/init_openshift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OPENSHIFT_TEMPLATE_DIR='./openshift-templates'
-CICD_TEMPLATES=(gogs gitlab nexus3 sonarqube)
+CICD_TEMPLATES=(gogs gitlab nexus3 sonarqube easymock kelk)
 OPENSHIFT_TEMPLATES=(redis-persistent postgresql-persistent mysql-persistent jenkins-ephemeral)
 
 # create project CI
