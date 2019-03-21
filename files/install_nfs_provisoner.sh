89bb92999a7c0dfcb4e3320797ca032796eeb2ab
xiaohua.pan <pxhua@yahoo.cn>
2019-03-21 11:09:03 +0800

    up for nfs install

--- files/install_nfs_provisoner.sh
+++ files/install_nfs_provisoner.sh
@@ -3,3 +3,4 @@ oc create -f ./files/nfs-provisoner/rbac.yaml
 oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:default:nfs-client-provisioner
 oc create -f ./files/nfs-provisoner/deployment.yaml
 oc create -f ./files/nfs-provisoner/class.yaml
+echo "success"
